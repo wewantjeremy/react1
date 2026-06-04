cbabbb659c0f85d9b0fe837b986aca02987c664c
wewantjeremy <dcmwise@gmail.com>
2026-06-04 17:20:58 -0400

    integration test

--- ecommerce-project/src/utils/money.test.js
+++ ecommerce-project/src/utils/money.test.js
@@ -1,6 +1,7 @@
-import { it, expect } from 'vitest'
+import { it, expect, describe } from 'vitest'
 import { formatMoney } from './money'
 
+describe('formatMoney', () => {
 it('formats 1999 as $19.99', () => {
   expect(formatMoney(1999)).toBe('$19.99')
 })
@@ -8,4 +9,5 @@ it('formats 1999 as $19.99', () => {
 it('displays 2 decimals', () => {
   expect(formatMoney(1090)).toBe('$10.90')
   expect(formatMoney(100)).toBe('$1.00')
+})
 })
